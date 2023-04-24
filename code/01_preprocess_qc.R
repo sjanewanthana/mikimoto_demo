c06b217c1035bd9c542fc73f101aa3457d867604
IntoxicateS <intoxicate@mm-dhcp-124-039.naist.jp>
2023-04-24 19:15:00 +0900

    Import count data and meta data

--- code/01_preprocess_qc.R
+++ code/01_preprocess_qc.R
@@ -1,4 +1,4 @@
-setwd("/Users/intoxicate/Desktop/mikimoto_demo")
+setwd("/Users/intoxicate/Desktop/mikimoto_demo/raw_data")
 library(tidyverse)
 library(dplyr)
 library(scater)
@@ -6,3 +6,10 @@ library(SingleCellExperiment)
 library(AnnotationDbi)
 library(org.Mm.eg.db)
 library(EnsDb.Mmusculus.v79)
+
+## --------------------------------------------------
+## (1) import count data and (2) meta data
+## --------------------------------------------------
+merge_count_data <- read.csv("merge_df_transposed_count.csv", header=T,row.names=1 ) 
+meta <- read.csv("merge_annotation.csv", header=T)
+
